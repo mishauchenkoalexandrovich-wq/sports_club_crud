6c69e5652ef667a38bc588753fe08f77a03a2726
Михайло <mishauchenkoalexandrovich@gmail.com>
2026-01-05 21:00:10 +0000

    Оновлено session_edit.php: додано календарик та підказки для валідації

--- app/views/session_edit.php
+++ app/views/session_edit.php
@@ -11,24 +11,54 @@ $sessions = $dao->getAllSessions();
 $current = array_filter($sessions, fn($s) => $s['id'] == $id);
 $current = reset($current);
 
+$error = "";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $dao->updateSession($id, $_POST['title'], $_POST['date'], $_POST['trainer_id']);
-    header("Location: ?action=sessions_list");
-    exit;
+    $title = trim($_POST['title'] ?? '');
+    $date = trim($_POST['date'] ?? '');
+    $trainer_id = $_POST['trainer_id'] ?? '';
+
+    // Валідація
+    if ($title === '') {
+        $error = "Назва не може бути порожньою";
+    } elseif ($date === '') {
+        $error = "Дата не може бути порожньою";
+    } elseif (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $date)) {
+        $error = "Некоректний формат дати (YYYY-MM-DD)";
+    } elseif ($trainer_id === '') {
+        $error = "Потрібно вибрати тренера";
+    }
+
+    // Якщо немає помилок – оновлюємо
+    if ($error === "") {
+        $dao->updateSession($id, $title, $date, $trainer_id);
+        header("Location: ?action=sessions_list");
+        exit;
+    }
 }
 ?>
 
 <h1>Редагувати тренування</h1>
 <form method="post">
-    Назва: <input type="text" name="title" value="<?= $current['title'] ?>"><br>
-    Дата: <input type="date" name="date" value="<?= $current['date'] ?>"><br>
+    Назва: <input type="text" name="title" value="<?= htmlspecialchars($current['title']) ?>"><br>
+    Дата: <input type="date" name="date" value="<?= htmlspecialchars($current['date']) ?>"><br>
+    <?php if ($error !== "" && strpos($error, 'Дата') !== false): ?>
+        <small style="color:red;"><?= $error ?></small><br>
+    <?php endif; ?>
     Тренер:
     <select name="trainer_id">
         <?php foreach ($trainers as $t): ?>
             <option value="<?= $t['id'] ?>" <?= $t['name'] == $current['trainer_name'] ? 'selected' : '' ?>>
-                <?= $t['name'] ?>
+                <?= htmlspecialchars($t['name']) ?>
             </option>
         <?php endforeach; ?>
     </select><br>
+    <?php if ($error !== "" && strpos($error, 'Тренер') !== false): ?>
+        <small style="color:red;"><?= $error ?></small><br>
+    <?php endif; ?>
     <button type="submit">Оновити</button>
 </form>
+
+<?php if ($error !== "" && strpos($error, 'Назва') !== false): ?>
+    <p style="color:red;"><?= $error ?></p>
+<?php endif; ?>
--- app/views/trainers_list.php
+++ app/views/trainers_list.php
@@ -8,15 +8,16 @@ $trainers = $dao->getAllTrainers();
 <a href="?action=trainer_add">Додати тренера</a>
 <table border="1">
     <tr><th>ID</th><th>Ім'я</th><th>Спеціалізація</th><th>Дії</th></tr>
-    <?php foreach ($trainers as $t): ?>
-        <tr>
-            <td><?= $t['id'] ?></td>
-            <td><?= $t['name'] ?></td>
-            <td><?= $t['specialization'] ?></td>
-            <td>
-                <a href="?action=trainer_edit&id=<?= $t['id'] ?>">Редагувати</a> |
-                <a href="?action=trainer_delete&id=<?= $t['id'] ?>">Видалити</a>
-            </td>
-        </tr>
-    <?php endforeach; ?>
+   <?php foreach ($trainers as $t): ?>
+    <tr>
+        <td><?= (int)$t['id'] ?></td>
+        <td><?= htmlspecialchars($t['name'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') ?></td>
+        <td><?= htmlspecialchars($t['specialization'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') ?></td>
+        <td>
+            <a href="?action=trainer_edit&id=<?= (int)$t['id'] ?>">Редагувати</a> |
+            <a href="?action=trainer_delete&id=<?= (int)$t['id'] ?>">Видалити</a>
+        </td>
+    </tr>
+<?php endforeach; ?>
+
 </table>
